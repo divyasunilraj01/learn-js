f57b539b2ac7879221a92c37c4b2a2a9b8386058
Divya <glishasunil@gmail.com>
2024-03-28 22:32:28 +0000

    logical updated

--- Logicalop.js
+++ Logicalop.js
@@ -19,3 +19,32 @@ let b = "Pen"
 let c = "Pencil"
 let data4 = b>c
 console.log(data4);
+// assignment operator
+let num1 = 10
+let num2 = 10
+let data5 = num1=num2
+console.log(data5);
+// equality operator
+let num3 = 10
+let num4 = 10
+let data6 = num3==num4
+console.log(data6);
+// strict equality operator
+let num5 = 10
+let num6 = 10
+let data7 = num5===num6
+console.log(data7);
+// debugging is removing bugs
+// coding is writing code is also making bugs
+let p = 5, q = 6, r = 7
+
+let result = p<q && q<r
+console.log(result)
+
+let result1 = p<q || q<r
+console.log(result1)
+// for AND &&
+// for OR ||
+// for NOT!
+// for XOR ^
+// using truth table
