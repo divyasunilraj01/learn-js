ed75ef1a42b81ca69904d205e9d1dcdce3f3ce7f
Divya <glishasunil@gmail.com>
2024-03-28 22:17:47 +0000

    history and operational

---
+++ Logicalop.js
@@ -0,0 +1,21 @@
+// compare values and return true or false
+let data = 7<2
+console.log(data);
+
+let data1 = 7<=7
+console.log(data1);
+
+let x = 6
+let y = 6
+let data2 = x==y
+console.log(data2);
+let x1 ="Pen"
+let y1 ="Book"
+let data3 = x1>y1
+console.log(data3);
+// it compares ASCII values
+
+let b = "Pen"
+let c = "Pencil"
+let data4 = b>c
+console.log(data4);
---
+++ relationalop.js
@@ -0,0 +1,37 @@
+//to compare two values
+console.log(10>20)
+let data = 7>6
+console.log(data)
+let data1 = 6<=6
+console.log(data1)
+let x = 10
+let y = 10
+let data2 = x==y
+console.log(data2)
+
+let x1 ="Pen"
+let y1 ="Book"
+let data3 = x1>y1
+console.log(data3)
+// compares ASCII values compare each letters that is p is grater than b and so on
+
+let b = "2"
+let c = 3
+let data4 = b>c
+console.log(data4)
+// here string is converted into number
+// strictly equal 
+// here string is not equal to number
+let d="3"
+let e=3
+let data5 = d===e
+console.log(data5)
+// strictly not equal
+let f="3"
+let g=3
+let data6 = f!==g
+console.log(data6)
+
+
+
+
