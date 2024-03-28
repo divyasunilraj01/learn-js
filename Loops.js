795fc5f82561d2096352b2c0395f7f7c15b2d0e3
Divya <glishasunil@gmail.com>
2024-03-28 23:47:54 +0000

    objects

---
+++ Loops.js
@@ -0,0 +1,39 @@
+// while loop, for loop, do while loop
+//DRY means do not repeat yourself
+//while loop
+// report this statement 5 times
+// intialize, condition, increment/decrement
+
+let i = 1;
+while (i<=5) {
+    console.log("Hello", i);
+    i++;
+}
+// post increment
+// pre increment ++i
+// post decrement i--
+// pre decrement --i
+
+// do while loop
+let j = 6;
+do {
+    console.log("hi", j);
+    j++;
+}
+while (j<=5)
+
+// for loop
+// for (let i=1; i<=5; i++) {
+    // console.log("Hello", i);
+// }
+// nested loops
+// for (let i=1; i<=5; i++) {
+    // console.log("Hello", i);
+    // for (let j=1; j<=5; j++) {
+        // console.log("Hi", j);
+    // }
+// }
+// if you know starting and ending point use for loop
+// if you don't know starting and ending point use while loop but you know the condition
+
+
---
+++ object.js
@@ -0,0 +1,15 @@
+// object has certain properties and methods
+// object literal
+// object has keys and values
+let input ='Divya'
+let alien  = {
+    name: "Sunil",
+    age: 23,
+    gender: "Male",
+    address: "Mumbai",
+    hobbies: ["Reading", "Coding", "Playing"]
+}
+console.log(alien.address)
+console.log(alien.hobbies[1])
+console.log(alien.hobbies.length)
+console.log(alien[input]);
